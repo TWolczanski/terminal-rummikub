e814e964d49b115732ff5be73cb6d2d5b9f3eeda
Tomasz Wołczański <tiamin@o2.pl>
2021-06-14 16:35:44 +0200

    test class

---
+++ src/test/java/rummikub/GameTest.java
@@ -0,0 +1,21 @@
+package rummikub;
+
+import static org.junit.Assert.*;
+// import org.junit.Test;
+
+public class GameTest {
+    @org.junit.Test
+    public static void main(String[] args){
+        Tile redTile = new Tile("red", 12);
+        Tile redTile2 = new Tile("red", 12);
+        Tile redTile3 = new Tile("red", 3);
+        Tile redTile4 = new Tile("blue", 12);
+        assertTrue(redTile.sameAs(redTile2));
+        assertTrue(redTile.sameColorAs(redTile2));
+        assertTrue(redTile.sameColorAs(redTile3));
+        assertTrue(!redTile.sameColorAs(redTile4));
+        assertTrue(redTile.sameAs("r12"));
+        assertTrue(!redTile.sameAs("r7"));
+        assertTrue(!redTile.sameAs("b12"));
+    }
+}
